f16159e92c4107ec4b3c5b4a3b11f93cbd12a8d7
Charles Northup <cjnorth40@gmail.com>
2014-03-11 11:23:33 -0500

    project complete

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -12,6 +12,8 @@
 @property IBOutlet UITextField *myNumber;
 @property (strong, nonatomic) IBOutlet UILabel *myMultiplier;
 @property (strong, nonatomic) IBOutlet UILabel *myAnswer;
+//sender of mySlider is the value of the slider in float form
+- (IBAction)mySlider:(UISlider *)sender;
 
 @end
 
@@ -27,6 +29,9 @@
 
 - (IBAction)onCalculateButtonPressed:(id)sender {
     
+/*
+*/
+    
     int number = [self.myNumber.text integerValue];
     int number2 = [self.myMultiplier.text integerValue];
     int solved = number * number2;
@@ -34,12 +39,32 @@
     if (solved > 20) {
         self.view.backgroundColor = [UIColor greenColor];
     }
+    else{
+        self.view.backgroundColor = [UIColor whiteColor];
+    }
 }
+
+/*sets myMultiplier text to the same as value of mySlider, but the (int)round(sender.value)
+turns my float value of mySlider into an int which is needed because my equation uses
+an int*/
+- (IBAction)mySlider:(UISlider *)sender {
+    self.myMultiplier.text = [NSString stringWithFormat:@"%d", (int)round(sender.value)];
+}
+
+- (IBAction)doneWithImput:(id)sender {
+    [self.myNumber endEditing:YES];
+}
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
 
 @end
